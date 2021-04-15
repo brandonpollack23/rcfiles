d3b9b393dfcda4b8a773b3eabc1b35931b2a4115
Brandon Pollack <brandonpollack23@gmail.com>
2021-04-14 20:31:05 -0400

    Initial Doom configuration done.

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Brandon Pollack"
+      user-mail-address "brandonpollack23@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -52,3 +52,10 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+;; Begin Custom Config
+
+;; imap mappings
+(use-package! evil-escape
+  :init
+  (setq evil-escape-key-sequence "jj"))
