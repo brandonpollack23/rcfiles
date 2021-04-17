98d8082ef493e588f292550a029b9bb9c4abe252
Brandon Pollack <brandonpollack23@gmail.com>
2021-04-16 22:49:21 -0400

    Turn on mu4e

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -93,7 +93,7 @@
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
-       ;;lsp
+       lsp
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
@@ -169,9 +169,9 @@
        ;;yaml              ; JSON, but readable
 
        :email
-       ;;(mu4e +gmail)
+       (mu4e +gmail)
        ;;notmuch
-       ;;(wanderlust +gmail)
+       ;; (wanderlust +gmail)
 
        :app
        ;;calendar
