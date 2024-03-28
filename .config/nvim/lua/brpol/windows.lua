5c2aa207788bf7b50168a6efe30ebcd7b2bc70ac
Brandon Pollack <brandonpollack23@gmail.com>
2024-03-28 16:40:43 +0900

    fix windows home

--- .config/nvim/lua/brpol/windows.lua
+++ .config/nvim/lua/brpol/windows.lua
@@ -1,2 +1,3 @@
 -- Note that this assumes chocolatey is what installed sqlite
+vim.fn.setenv('HOME', os.getenv('userprofile'))
 vim.g.sqlite_clib_path = [[C:\ProgramData\chocolatey\lib\SQLite\tools\sqlite3.dll]]
