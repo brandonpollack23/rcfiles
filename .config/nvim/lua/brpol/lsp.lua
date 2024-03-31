a72dba81d5430ab4ce04de3ac7e48cb55a894c2b
Brandon Pollack <brandonpollack23@gmail.com>
2024-03-31 11:39:48 +0200

    fixed autocompletion

--- .config/nvim/lua/brpol/lsp.lua
+++ .config/nvim/lua/brpol/lsp.lua
@@ -1,5 +1,6 @@
 local lsp_zero = require('lsp-zero')
 local lspconfig = require('lspconfig')
+local luasnip = require('luasnip')
 
 lsp_zero.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
@@ -86,19 +87,47 @@ require('mason-lspconfig').setup({
 local cmp = require('cmp')
 local cmp_select = { behaviour = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp_zero.defaults.cmp_mappings({
-  ['C-k'] = cmp.mapping.select_prev_item(cmp_select),
-  ['C-j'] = cmp.mapping.select_next_item(cmp_select),
-  ['C-p'] = cmp.mapping.select_prev_item(cmp_select),
-  ['C-n'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-Space>'] = cmp.mapping.complete(),
+  ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
 
-  ['C-y'] = cmp.mapping.confirm({ select = true }),
-  ['C-e'] = cmp.mapping.abort(),
+  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
 
-  ['C-Space'] = cmp.mapping.complete(),
+
+  ['<C-e>'] = cmp.mapping.close(),
+  ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+  ['<C-d>'] = cmp.mapping.scroll_docs(4),
+  ['<CR>'] = cmp.mapping.confirm({
+    behavior = cmp.ConfirmBehavior.Replace,
+    select = false,
+  }),
+
+  ['<Tab>'] = cmp.mapping(function(fallback)
+    if cmp.visible() then
+      cmp.select_next_item()
+    elseif luasnip.expand_or_jumpable() then
+      luasnip.expand_or_jump()
+    else
+      fallback()
+    end
+  end, { 'i', 's' }),
+
+  ['<S-Tab>'] = cmp.mapping(function(fallback)
+    if cmp.visible() then
+      cmp.select_prev_item()
+    elseif luasnip.jumpable(-1) then
+      luasnip.jump(-1)
+    else
+      fallback()
+    end
+  end, { 'i', 's' }),
 })
+
 cmp.setup({
   window = {},
-  mapping = cmp.mapping.preset.insert(cmp_mappings)
+  mapping = cmp_mappings,
 })
 
 lsp_zero.set_preferences({
