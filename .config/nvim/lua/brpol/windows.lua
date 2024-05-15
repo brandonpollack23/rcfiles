268581329b69d8ec313a872027e6277476be33fb
Brandon Pollack <brandonpollack23@gmail.com>
2024-05-16 01:25:03 +0900

    fixed neovide

--- .config/nvim/lua/brpol/windows.lua
+++ .config/nvim/lua/brpol/windows.lua
@@ -1,3 +1,6 @@
 -- Note that this assumes chocolatey is what installed sqlite
 vim.fn.setenv('HOME', os.getenv('userprofile'))
 vim.g.sqlite_clib_path = [[C:\ProgramData\chocolatey\lib\SQLite\tools\sqlite3.dll]]
+vim.g.neovide_cursor_animation_length = 0.0
+vim.g.neovide_cursor_trail_size = 0.0
+vim.g.neovide_scroll_animation_length = 0.0
