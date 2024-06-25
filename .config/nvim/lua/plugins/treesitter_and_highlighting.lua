bcb4e1a5d9ce279b279027b980b8560edd452f3c
Brandon Pollack <brandonpollack23@gmail.com>
2024-06-25 04:35:40 +0000

    treesitter changes

--- .config/nvim/lua/plugins/treesitter_and_highlighting.lua
+++ .config/nvim/lua/plugins/treesitter_and_highlighting.lua
@@ -8,10 +8,32 @@ return {
     config = function()
       require('nvim-treesitter.configs').setup({
         -- A list of parser names, or "all" https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#supported-languages
-        ensure_installed = 'all',
+        ensure_installed = {
+          'awk',
+          'bash',
+          'c',
+          'c_sharp',
+          'clojure',
+          'cpp',
+          'css',
+          'gitignore',
+          'go',
+          'gomod',
+          'html',
+          'javascript',
+          'json',
+          'json5',
+          'jsonc',
+          'kotlin',
+          'lua',
+          'python',
+          'rust',
+          'typescript',
+          'vim',
+        },
 
         -- Install parsers synchronously (only applied to `ensure_installed`)
-        sync_install = false,
+        sync_install = true,
 
         -- Automatically install missing parsers when entering buffer
         -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
