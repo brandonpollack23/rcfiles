bbbab2247f50f590d676fcb241d5f174f30e01a4
Brandon Pollack <brandonpollack23@gmail.com>
2024-06-28 13:35:10 +0900

    more snip examples

--- .config/nvim/lua/brpol/snippets/example_snippets.lua
+++ .config/nvim/lua/brpol/snippets/example_snippets.lua
@@ -3,6 +3,13 @@ local s = luasnip.snippet
 local t = luasnip.text_node
 local i = luasnip.insert_node
 
+local extras = require('luasnip.extras')
+local rep = extras.rep
+
+local fmt = require('luasnip.extras.fmt').fmt
+
+-- There are even more, function nodes, choice nodes, dynamic nodes, etc. https://github.com/L3MON4D3/LuaSnip/blob/master/DOC.md
+
 -- insert_node is for tabbing between placeholders in the order specified, 0 means where to go when done.
 
 luasnip.add_snippets('lua', {
@@ -14,4 +21,17 @@ luasnip.add_snippets('lua', {
     i(2, 'world'),
     t('!')
   }),
+  s('repeat_example', {
+    t('<'), i(1), t('>'),
+    t('</'), rep(1), t('>')
+  }),
+  s('format_example', {
+    -- Curlies are where things go, like rust, Double curly is escaped curly.
+    fmt([[
+\begin{{{}}}
+  {}
+\end{{{}}}
+    ]], { i(1), i(2), rep(1)
+    })
+  })
 })
