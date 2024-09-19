296e4c0763a79ad553a74f4686c63094661b11df
Brandon Pollack <brandonpollack23@gmail.com>
2024-09-19 21:13:08 +0900

    disable crap that pisses me off

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -4,7 +4,7 @@ return {
     config = function()
       local gs = require('gitsigns')
       gs.setup {
-        current_line_blame = true,
+        current_line_blame = false, --buggy because of https://github.com/lewis6991/gitsigns.nvim/issues/368
         current_line_blame_opts = {
           virt_text_pos = 'right_align',
           delay = 250,
