4e5fde30e3e6feb85572b46aaf038c1ea0fd1fc1
Brandon Pollack <brandonpollack23@gmail.com>
2024-09-24 10:25:12 +0900

    switch back to old diff tool and set up conform to only format changes

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -89,17 +89,54 @@ return {
   {
     'stevearc/conform.nvim',
     opts = {},
-    dependencies = { 'folke/which-key.nvim' },
+    dependencies = {
+      'folke/which-key.nvim',
+      'lewis6991/gitsigns.nvim',
+    },
     config = function()
       require('conform').setup({
         formatters_by_ft = {
           python = { 'black', 'isort' },
           javascript = { { 'prettierd', 'prettier' } },
         },
-        format_on_save = {
-          timeout_ms = 5000,
-          lsp_fallback = true,
-        },
+        -- format_on_save = {
+        --   timeout_ms = 5000,
+        --   lsp_fallback = true,
+        -- },
+      })
+
+      -- Only format diffs on save to prevent noisy git history:
+      local function format_modified(timeout_ms)
+        local uv = vim.uv or vim.loop
+        local start = uv.hrtime() / 1e6
+
+        local hunks = require('gitsigns').get_hunks()
+        local format = require('conform').format
+
+        for i = #hunks, 1, -1 do
+          local remaining = timeout_ms - (uv.hrtime() / 1e6 - start)
+          if remaining < 0 then
+            vim.notify('Timed out formatting diffs', 'error')
+            return
+          end
+
+          local hunk = hunks[i]
+          if hunk ~= nil and hunk.type ~= 'delete' then
+            local start = hunk.added.start
+            local last = start + hunk.added.count
+            -- nvim_buf_get_lines uses zero-based indexing -> subtract from last
+            local last_hunk_line = vim.api.nvim_buf_get_lines(0, last - 2, last - 1, true)[1]
+            local range = { start = { start, 0 }, ['end'] = { last - 1, last_hunk_line:len() } }
+            format({ range = range })
+          end
+        end
+      end
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        pattern = '*',
+        callback = function(args)
+          vim.notify('Formatting diffs with conform and custom fn', 'info')
+          format_modified(5000)
+        end,
       })
 
       require('which-key').register({
