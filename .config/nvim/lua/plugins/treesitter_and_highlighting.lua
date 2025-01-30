31470f77a380b6788c030e4763affb8909267e08
Brandon Pollack <brandonpollack23@gmail.com>
2025-01-30 14:28:12 +0100

    remove stuff I dont use now

--- .config/nvim/lua/plugins/treesitter_and_highlighting.lua
+++ .config/nvim/lua/plugins/treesitter_and_highlighting.lua
@@ -164,18 +164,8 @@ return {
               ['[d'] = '@conditional.outer',
             }
           },
-
-          -- disable = function(lang, buf)
-          --   local max_filesize = 100 * 1024 -- 100 KB
-          --   local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-          --   if ok and stats and stats.size > max_filesize then
-          --     return true
-          --   end
-          -- end,
         },
       })
-      --
-      -- https://github.com/RRethy/vim-illuminate?tab=readme-ov-file#highlight-groups
     end,
   },
   {
