67de5e92d8bb4917d503d01c98f69291c61e2048
Brandon Pollack <brandonpollack23@gmail.com>
2025-02-07 21:47:15 +0100

    nvim markdown

--- .config/nvim/lua/plugins/themes_and_ui.lua
+++ .config/nvim/lua/plugins/themes_and_ui.lua
@@ -274,14 +274,14 @@ return {
         override = {
           ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
           ['vim.lsp.util.stylize_markdown'] = true,
-          ['cmp.entry.get_documentation'] = true,   -- requires hrsh7th/nvim-cmp
+          ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
         },
         presets = {
-          bottom_search = true,           -- use a classic bottom cmdline for search
-          command_palette = true,         -- position the cmdline and popupmenu together
-          long_message_to_split = true,   -- long messages will be sent to a split
-          inc_rename = false,             -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = false,         -- add a border to hover docs and signature help
+          bottom_search = true,         -- use a classic bottom cmdline for search
+          command_palette = true,       -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false,       -- add a border to hover docs and signature help
         },
         lsp = {
           signature = {
@@ -403,4 +403,13 @@ return {
       })
     end
   },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  }
 }
