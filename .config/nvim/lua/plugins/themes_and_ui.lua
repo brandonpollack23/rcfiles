419ad8a0f974a1a4477f6b7dc4e9a23feb0b627a
Brandon Pollack <brandonpollack23@gmail.com>
2025-05-17 10:54:51 +0900

    neovimmap

--- .config/nvim/lua/plugins/themes_and_ui.lua
+++ .config/nvim/lua/plugins/themes_and_ui.lua
@@ -331,68 +331,122 @@ return {
   },
 
   -- Pretty scrollbar with information.
+  -- {
+  --   'lewis6991/satellite.nvim',
+  --   dependencies = { 'lewis6991/gitsigns.nvim' },
+  --   config = function()
+  --     require('satellite').setup({
+  --       current_only = false,
+  --       winblend = 50,
+  --       zindex = 40,
+  --       excluded_filetypes = {},
+  --       width = 16,
+  --       handlers = {
+  --         cursor = {
+  --           enable = true,
+  --           -- Supports any number of symbols
+  --           symbols = { '⎺', '⎻', '⎼', '⎽' }
+  --           -- symbols = { '⎻', '⎼' }
+  --           -- Highlights:
+  --           -- - SatelliteCursor (default links to NonText
+  --         },
+  --         search = {
+  --           enable = true,
+  --           -- Highlights:
+  --           -- - SatelliteSearch (default links to Search)
+  --           -- - SatelliteSearchCurrent (default links to SearchCurrent)
+  --         },
+  --         diagnostic = {
+  --           enable = true,
+  --           signs = { '-', '=', '≡' },
+  --           min_severity = vim.diagnostic.severity.HINT,
+  --           -- Highlights:
+  --           -- - SatelliteDiagnosticError (default links to DiagnosticError)
+  --           -- - SatelliteDiagnosticWarn (default links to DiagnosticWarn)
+  --           -- - SatelliteDiagnosticInfo (default links to DiagnosticInfo)
+  --           -- - SatelliteDiagnosticHint (default links to DiagnosticHint)
+  --         },
+  --         gitsigns = {
+  --           enable = true,
+  --           signs = { -- can only be a single character (multibyte is okay)
+  --             add = '│',
+  --             change = '│',
+  --             delete = '-',
+  --           },
+  --           -- Highlights:
+  --           -- SatelliteGitSignsAdd (default links to GitSignsAdd)
+  --           -- SatelliteGitSignsChange (default links to GitSignsChange)
+  --           -- SatelliteGitSignsDelete (default links to GitSignsDelete)
+  --         },
+  --         marks = {
+  --           enable = true,
+  --           show_builtins = false, -- shows the builtin marks like [ ] < >
+  --           key = 'm'
+  --           -- Highlights:
+  --           -- SatelliteMark (default links to Normal)
+  --         },
+  --         quickfix = {
+  --           signs = { '-', '=', '≡' },
+  --           -- Highlights:
+  --           -- SatelliteQuickfix (default links to WarningMsg)
+  --         }
+  --       },
+  --     })
+  --   end
+  -- },
+  -- VScode like minimap
   {
-    'lewis6991/satellite.nvim',
-    dependencies = { 'lewis6991/gitsigns.nvim' },
-    config = function()
-      require('satellite').setup({
-        current_only = false,
-        winblend = 50,
-        zindex = 40,
-        excluded_filetypes = {},
-        width = 16,
-        handlers = {
-          cursor = {
-            enable = true,
-            -- Supports any number of symbols
-            symbols = { '⎺', '⎻', '⎼', '⎽' }
-            -- symbols = { '⎻', '⎼' }
-            -- Highlights:
-            -- - SatelliteCursor (default links to NonText
-          },
-          search = {
-            enable = true,
-            -- Highlights:
-            -- - SatelliteSearch (default links to Search)
-            -- - SatelliteSearchCurrent (default links to SearchCurrent)
-          },
-          diagnostic = {
-            enable = true,
-            signs = { '-', '=', '≡' },
-            min_severity = vim.diagnostic.severity.HINT,
-            -- Highlights:
-            -- - SatelliteDiagnosticError (default links to DiagnosticError)
-            -- - SatelliteDiagnosticWarn (default links to DiagnosticWarn)
-            -- - SatelliteDiagnosticInfo (default links to DiagnosticInfo)
-            -- - SatelliteDiagnosticHint (default links to DiagnosticHint)
-          },
-          gitsigns = {
-            enable = true,
-            signs = { -- can only be a single character (multibyte is okay)
-              add = '│',
-              change = '│',
-              delete = '-',
-            },
-            -- Highlights:
-            -- SatelliteGitSignsAdd (default links to GitSignsAdd)
-            -- SatelliteGitSignsChange (default links to GitSignsChange)
-            -- SatelliteGitSignsDelete (default links to GitSignsDelete)
-          },
-          marks = {
-            enable = true,
-            show_builtins = false, -- shows the builtin marks like [ ] < >
-            key = 'm'
-            -- Highlights:
-            -- SatelliteMark (default links to Normal)
-          },
-          quickfix = {
-            signs = { '-', '=', '≡' },
-            -- Highlights:
-            -- SatelliteQuickfix (default links to WarningMsg)
-          }
+    'Isrothy/neominimap.nvim',
+    dependencies = { 'lewis6991/gitsigns.nvim', 'nvim-treesitter/nvim-treesitter' },
+    version = 'v3.x.x',
+    lazy = false, -- NOTE: NO NEED to Lazy load
+    keys = {
+      -- Global Minimap Controls
+      { '<leader>mm',  '<cmd>Neominimap Toggle<cr>',      desc = 'Toggle global minimap' },
+      { '<leader>mo',  '<cmd>Neominimap Enable<cr>',      desc = 'Enable global minimap' },
+      { '<leader>mc',  '<cmd>Neominimap Disable<cr>',     desc = 'Disable global minimap' },
+      { '<leader>mr',  '<cmd>Neominimap Refresh<cr>',     desc = 'Refresh global minimap' },
+
+      -- Window-Specific Minimap Controls
+      { '<leader>mwt', '<cmd>Neominimap WinToggle<cr>',   desc = 'Toggle minimap for current window' },
+      { '<leader>mwr', '<cmd>Neominimap WinRefresh<cr>',  desc = 'Refresh minimap for current window' },
+      { '<leader>mwo', '<cmd>Neominimap WinEnable<cr>',   desc = 'Enable minimap for current window' },
+      { '<leader>mwc', '<cmd>Neominimap WinDisable<cr>',  desc = 'Disable minimap for current window' },
+
+      -- Tab-Specific Minimap Controls
+      { '<leader>mtt', '<cmd>Neominimap TabToggle<cr>',   desc = 'Toggle minimap for current tab' },
+      { '<leader>mtr', '<cmd>Neominimap TabRefresh<cr>',  desc = 'Refresh minimap for current tab' },
+      { '<leader>mto', '<cmd>Neominimap TabEnable<cr>',   desc = 'Enable minimap for current tab' },
+      { '<leader>mtc', '<cmd>Neominimap TabDisable<cr>',  desc = 'Disable minimap for current tab' },
+
+      -- Buffer-Specific Minimap Controls
+      { '<leader>mbt', '<cmd>Neominimap BufToggle<cr>',   desc = 'Toggle minimap for current buffer' },
+      { '<leader>mbr', '<cmd>Neominimap BufRefresh<cr>',  desc = 'Refresh minimap for current buffer' },
+      { '<leader>mbo', '<cmd>Neominimap BufEnable<cr>',   desc = 'Enable minimap for current buffer' },
+      { '<leader>mbc', '<cmd>Neominimap BufDisable<cr>',  desc = 'Disable minimap for current buffer' },
+
+      ---Focus Controls
+      { '<leader>mf',  '<cmd>Neominimap Focus<cr>',       desc = 'Focus on minimap' },
+      { '<leader>mu',  '<cmd>Neominimap Unfocus<cr>',     desc = 'Unfocus minimap' },
+      { '<leader>ms',  '<cmd>Neominimap ToggleFocus<cr>', desc = 'Switch focus on minimap' },
+    },
+    init = function()
+      -- The following options are recommended when layout == "float"
+      vim.opt.wrap = false
+      vim.opt.sidescrolloff = 36 -- Set a large value
+
+      --- Put your configuration here
+      ---@type Neominimap.UserConfig
+      vim.g.neominimap = {
+        auto_enable = true,
+        mark = {
+          enable = true,
         },
-      })
-    end
+        search = {
+          enable = true,
+        },
+      }
+    end,
   },
   {
     'MeanderingProgrammer/render-markdown.nvim',
