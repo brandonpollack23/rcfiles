5b8f492a1692d1f50bb5733138fc9a6584e3b5ef
Brandon Pollack <brandonpollack23@gmail.com>
2026-02-02 17:23:37 +0900

    jujutsu config

--- install.sh
+++ install.sh
@@ -296,6 +296,7 @@ function setup_home_dir() {
   ln -sfn "$RCFILES_DIR/.config/ghostty" "$HOME/.config/ghostty"
   ln -sfn "$RCFILES_DIR/.config/amp" "$HOME/.config/amp"
   ln -sfn "$RCFILES_DIR/.config/hypr" "$HOME/.config/hypr"
+  ln -sfn "$RCFILES_DIR/.config/jj" "$HOME/.config/jj"
   ln -sfn "$RCFILES_DIR/.cowfiles" "$HOME/.cowfiles"
   ln -sfn "$RCFILES_DIR/.cowrc" "$HOME/.cowrc"
   ln -sfn "$RCFILES_DIR/.gitconfig" "$HOME/.gitconfig"
