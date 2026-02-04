2cc64606f64c0014ceb2ffe1dce87f6af80f7778
Brandon Pollack <brandonpollack23@gmail.com>
2026-02-04 13:53:58 +0900

    fix flash to use two chars

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -19,13 +19,43 @@ return {
         "<leader><leader>w",
         mode = { "n", "x", "o" },
         function()
-          require("flash").jump({
-            pattern = "\\<", -- match word boundaries only
-            search = {
-              mode = "search",
-              max_length = 0,
-            },
-            label = { after = { 0, 0 } },
+          local Flash = require("flash")
+          local function format(opts)
+            return {
+              { opts.match.label1, "FlashMatch" },
+              { opts.match.label2, "FlashLabel" },
+            }
+          end
+          Flash.jump({
+            search = { mode = "search", max_length = 0 },
+            label = { after = false, before = { 0, 0 }, uppercase = false, format = format },
+            pattern = [[\<]],
+            action = function(match, state)
+              state:hide()
+              Flash.jump({
+                search = { max_length = 0 },
+                highlight = { matches = false },
+                label = { format = format },
+                matcher = function(win)
+                  return vim.tbl_filter(function(m)
+                    return m.label == match.label and m.win == win
+                  end, state.results)
+                end,
+                labeler = function(matches)
+                  for _, m in ipairs(matches) do
+                    m.label = m.label2
+                  end
+                end,
+              })
+            end,
+            labeler = function(matches, state)
+              local labels = state:labels()
+              for m, match in ipairs(matches) do
+                match.label1 = labels[math.floor((m - 1) / #labels) + 1]
+                match.label2 = labels[(m - 1) % #labels + 1]
+                match.label = match.label1
+              end
+            end,
           })
         end,
         desc = "Jump to Word",
@@ -34,14 +64,43 @@ return {
         "<leader><leader>b",
         mode = { "n", "x", "o" },
         function()
-          require("flash").jump({
-            pattern = "\\<", -- match word boundaries only
-            search = {
-              mode = "search",
-              max_length = 0,
-            },
-            label = { after = { 0, 0 } },
-            direction = "backward",
+          local Flash = require("flash")
+          local function format(opts)
+            return {
+              { opts.match.label1, "FlashMatch" },
+              { opts.match.label2, "FlashLabel" },
+            }
+          end
+          Flash.jump({
+            search = { mode = "search", max_length = 0, forward = false, wrap = false },
+            label = { after = false, before = { 0, 0 }, uppercase = false, format = format },
+            pattern = [[\<]],
+            action = function(match, state)
+              state:hide()
+              Flash.jump({
+                search = { max_length = 0 },
+                highlight = { matches = false },
+                label = { format = format },
+                matcher = function(win)
+                  return vim.tbl_filter(function(m)
+                    return m.label == match.label and m.win == win
+                  end, state.results)
+                end,
+                labeler = function(matches)
+                  for _, m in ipairs(matches) do
+                    m.label = m.label2
+                  end
+                end,
+              })
+            end,
+            labeler = function(matches, state)
+              local labels = state:labels()
+              for m, match in ipairs(matches) do
+                match.label1 = labels[math.floor((m - 1) / #labels) + 1]
+                match.label2 = labels[(m - 1) % #labels + 1]
+                match.label = match.label1
+              end
+            end,
           })
         end,
         desc = "Jump to Word",
