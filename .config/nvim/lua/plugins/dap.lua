f13ea9b04cd495adfc2758186fa61dc93afd8601
Brandon Pollack <brandonpollack23@gmail.com>
2026-02-17 11:04:20 +0900

    dap stuff

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -2,9 +2,26 @@ return {
   {
     "mfussenegger/nvim-dap",
     opts = function()
-      -- Always load .vscode/launch.json if it exists
+      local dap = require("dap")
+      -- assumes gdb is installed with the right version
+      -- adapter is a function so we can pass the program via -se,
+      -- ensuring symbols are loaded before DAP sets breakpoints
+      dap.adapters.gdb = function(callback, config)
+        local args = { "-i", "dap" }
+        if config.program then
+          table.insert(args, "-se")
+          table.insert(args, config.program)
+        end
+        callback({
+          type = "executable",
+          command = "gdb",
+          args = args,
+        })
+      end
+
+      -- Load .vscode/launch.json, mapping both cppdbg and gdb types to our gdb adapter
       local vscode = require("dap.ext.vscode")
-      vscode.load_launchjs(nil, {})
+      vscode.getconfigs()
     end,
   },
 }
