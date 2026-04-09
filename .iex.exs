06a8c68bf05ae219ca877b458d6901c7f212dda3
Brandon Pollack <brandonpollack23@gmail.com>
2026-04-09 18:46:11 +0900

    iex stuff

--- .iex.exs
+++ .iex.exs
@@ -19,3 +19,38 @@ IEx.configure(
   alive_prompt: "[%prefix] %node %counter>",
   inspect: [pretty: true, limit: :infinity, width: 80]
 )
+
+defmodule History do
+  def history do
+    :group_history.load()
+    # drop the history call itself
+    |> Enum.drop(1)
+    |> Enum.reverse()
+    |> Enum.with_index()
+    |> Enum.map(fn {cmd, idx} -> "#{idx}) #{cmd}" end)
+    |> Enum.join()
+    |> IO.puts()
+
+    IEx.dont_display_result()
+  end
+
+  def history(number) when is_integer(number) and number > 0 do
+    # subtract one for this command
+    hist = :group_history.load() |> Enum.drop(1)
+    l = hist |> length()
+
+    command = hist |> Enum.at(l - number - 1)
+
+    Code.eval_string(command)
+
+    IEx.dont_display_result()
+  end
+
+  def history(number) when is_integer(number) and number < 0 do
+    hist = :group_history.load() |> Enum.drop(2)
+    l = hist |> length()
+    history(l + number)
+  end
+end
+
+import History
